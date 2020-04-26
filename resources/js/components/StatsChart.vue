5dcab8f38d83506e673f60de1d0e9c272a311608
Chuck Cerrillo <cerrillo.chuck@abc.net.au>
2020-04-26 16:39:33 +1000

    Set default chart settings to linear / bar

--- resources/js/components/StatsChart.vue
+++ resources/js/components/StatsChart.vue
@@ -433,13 +433,13 @@
                         primary: {
                             color: this.options.colors[random1].bg,
                             border: this.options.colors[random1].border,
-                            type: 'line',
+                            type: 'bar',
                             scale: scaleType.primary,
                         },
                         secondary: {
                             color: this.options.colors[random2].bg,
                             border: this.options.colors[random2].border,
-                            type: 'line',
+                            type: 'bar',
                             scale: scaleType.secondary,
                         },
                     })
