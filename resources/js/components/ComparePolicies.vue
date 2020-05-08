c8658ec60b9403067f47269e9cc78ef0845ceb00
Chuck Cerrillo <cerrillo.chuck@abc.net.au>
2020-05-08 21:21:56 +1000

    Adjust govt response comparison table

--- resources/js/components/ComparePolicies.vue
+++ resources/js/components/ComparePolicies.vue
@@ -1,32 +1,11 @@
 <template>
     <div class="h-full">
-        <div class="m-4" v-if="1==2">
-
-            <div class="my-2 py-2 pb-4 px-4 bg-slab-primary rounded">
-                <span class="font-bold">Select date</span>
-                <v-date-picker
-                    v-model="date"
-                    :min-date="options.date.min"
-                    :max-date="options.date.max"
-                    :masks="{ data: ['YYYY-MM-DD', 'YYYY/MM/DD'],input: ['YYYY-MM-DD', 'YYYY/MM/DD'] }"
-                    :popover="{ placement: 'bottom', visibility: 'click' }">
-                    <button class="p-2 hover:bg-lightlabel text-white rounded focus:outline-none">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            class="w-4 h-4 fill-current">
-                            <path d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z" />
-                        </svg>
-                    </button>
-                </v-date-picker> {{moment(date).format('YYYY-MM-DD')}}
-                <vue-slider v-model="date" :data="dateSliderRange" :lazy="true" :adsorb="true" />
-            </div>
-        </div>
-        <div class="absolute top-0 left-0 right-0 bottom-0 m-4" style="position:absolute; top: 5.5rem">
+        <div class="absolute top-0 left-0 right-0 bottom-0 m-4" style="position:absolute; top: 4rem">
             <simplebar data-simplebar-auto-hide="false" class="w-full h-full">
                 <div class="flex justify-start rounded-t z-10 relative bg-slab-primary" style="min-width: 75rem;">
                     <div class="bg-slab-primary justify-start items-end border-lightslab w-36 text-xs font-bold flex-shrink-0">
-                        <div class="px-4 py-2 h-12"></div>
+                        <div class="px-4 py-2 h-20"></div>
+                        <div class="px-4 py-2">Stringency Index</div>
                     </div>
                     <div
                         class="border-l flex-shrink-0
@@ -37,14 +16,17 @@
                             <div class="px-4 py-2 text-2xl h-12 bg-slab-primary">
                                 <div class="truncate ... font-bold">{{row.name}}</div>
                             </div>
+                            <div class="px-4 py-2 h-24 text-5xl font-bold">
+                                <div v-if="!isNaN(row.stringencyindex)">{{row.stringencyindex | numeralFormat('0.00')}}</div>
+                                <div v-else>N/A</div>
+                            </div>
                         </div>
                      </div>
-                    <div class="px-4 py-2 h-20 border-l border-lightslab"></div>
+                    <div class="px-4 py-2 h-36 border-l border-lightslab"></div>
                 </div>
                 <simplebar data-simplebar-auto-hide="false" class="inner-scrollbar bg-slab rounded absolute inset-x-0 bottom-0" style="min-width: 75rem; position: absolute; top: 80px;" :style="'min-width: ' + (9 + (data.length * 16)) + 'em'">
                     <div class="flex justify-start rounded-t z-10 relative bg-slab">
                         <div class="bg-slab-primary justify-start items-end border-b border-lightslab w-36 text-xs font-bold flex-shrink-0">
-                            <div class="px-4 py-2 h-24">Stringency Index</div>
                             <div class="px-4 py-2 h-16 bg-darkslab border-b border-slab">Containment and closure</div>
                             <div class="px-4 py-2 h-24 border-b border-slab">C1 - Schools</div>
                             <div class="px-4 py-2 h-24 border-b border-slab">C2 - Workplaces</div>
@@ -55,13 +37,13 @@
                             <div class="px-4 py-2 h-24 border-b border-slab">C7 - Internal movements</div>
                             <div class="px-4 py-2 h-16 border-b border-slab">C8 - International travel</div>
                             <div class="px-4 py-2 h-16 bg-darkslab border-b border-slab">Economic response</div>
-                            <div class="px-4 py-2 h-24 border-b border-slab">E1 - Income support</div>
+                            <div class="px-4 py-2 h-28 border-b border-slab">E1 - Income support</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">E2 - Debt/contract relief</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">E3 - Fiscal measures</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">E4 - International support</div>
                             <div class="px-4 py-2 h-16 bg-darkslab border-b border-slab">Health systems</div>
                             <div class="px-4 py-2 h-24 border-b border-slab">H1 - Public information campaign</div>
-                            <div class="px-4 py-2 h-20 border-b border-slab">H2 - Testing policy</div>
+                            <div class="px-4 py-2 h-28 border-b border-slab">H2 - Testing policy</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">H3 - Contact tracing</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">H4 - Emergency healthcare investment</div>
                             <div class="px-4 py-2 h-20 border-b border-slab">H5 - COVID-19 vaccine investment</div>
@@ -77,10 +59,6 @@
                                      :class="index % 2 == 1 ? 'bg-slab-primary' : ''">
 
                                     <div class="w-full">
-                                        <div class="px-4 py-2 h-24 text-5xl font-bold">
-                                            <div v-if="!isNaN(row.stringencyindex)">{{row.stringencyindex | numeralFormat('0.00')}}</div>
-                                            <div v-else>N/A</div>
-                                        </div>
 
                                         <div class="px-4 py-2 h-16 bg-darkslab border-b border-lightslab">
                                         </div>
@@ -151,7 +129,7 @@
                                         <div class="px-4 py-2 h-16 bg-darkslab border-b border-lightslab">
                                         </div>
 
-                                        <div class="px-4 py-2 h-24 border-b border-lightslab">
+                                        <div class="px-4 py-2 h-28 border-b border-lightslab">
                                             <div v-if="row.latest.E1.value.length > 60" class="text-xs">{{row.latest.E1.value}}</div>
                                             <div v-else-if="row.latest.E1.value.length > 30" class="text-sm">{{row.latest.E1.value}}</div>
                                             <div v-else-if="row.latest.E1.value">{{row.latest.E1.value}}</div>
@@ -187,7 +165,7 @@
                                             <div v-if="row.latest.H1.target" class="text-xs text-lightlabel">({{row.latest.H1.target}})</div>
                                         </div>
 
-                                        <div class="px-4 py-2 h-20 border-b border-lightslab">
+                                        <div class="px-4 py-2 h-28 border-b border-lightslab">
                                             <div v-if="row.latest.H2.value.length > 60" class="text-xs">{{row.latest.H2.value}}</div>
                                             <div v-else-if="row.latest.H2.value.length > 30" class="text-sm">{{row.latest.H2.value}}</div>
                                             <div v-else-if="row.latest.H2.value">{{row.latest.H2.value}}</div>
